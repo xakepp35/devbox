a2220d2ee4d8ca719aa2068e5c372a3cd8131627
g0p43r-dev <snovichkov@anykey.com>
2025-09-26 18:39:36 +0300

    Remove version tag

--- gitlab/run.sh
+++ gitlab/run.sh
@@ -1,1 +1,14 @@
-docker compose -f docker-compose.yml --env-file .env up -d
+#!/bin/sh
+
+
+# Generate password
+generate_password(){
+    local length=${1:-12}   # Default length 12 symbols
+    # Generate password using /dev/urandom, base64, and tr to remove unwanted chars
+    tr -dc 'A-Za-z0-9!@#$%^&*()_+-=' < /dev/urandom | head -c "$length"
+    echo
+}
+
+password=$(generate_password 16)
+
+echo $password
