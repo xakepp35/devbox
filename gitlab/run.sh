869ff75e34b999e8d36c1e1f91af9e5a87cad56c
g0p43r-dev <snovichkov@anykey.com>
2025-09-26 18:41:49 +0300

    Use default run.sh script

--- gitlab/run.sh
+++ gitlab/run.sh
@@ -1,14 +1,1 @@
-#!/bin/sh
-
-
-# Generate password
-generate_password(){
-    local length=${1:-12}   # Default length 12 symbols
-    # Generate password using /dev/urandom, base64, and tr to remove unwanted chars
-    tr -dc 'A-Za-z0-9!@#$%^&*()_+-=' < /dev/urandom | head -c "$length"
-    echo
-}
-
-password=$(generate_password 16)
-
-echo $password
+docker compose -f docker-compose.gitlab.yml --env-file .env up -d
